8835552f423df1efc76a26df7df11b1957ea92c9
Montana Mendy <montana@linux.com>
2022-01-06 23:44:17 -0800

    refactor spearman

--- spearman.m
+++ spearman.m
@@ -1,7 +1,7 @@
 function plotScatterCircleColor(vx, vy, vc, figNum, labelX, labelY)
 
-vy=vy(vx>0);
-vx=vx(vx>0);
+vy = vy(vx > 0);
+vx = vx(vx > 0);
 
 myFigure(figNum)
 scatter(vx, vy, 100, vc, 'filled')
@@ -10,10 +10,10 @@ xlabel(labelX)
 ylabel(labelY)
 colormap('jet')
 caxis([0 100])
-colorbar('Ytick',0:20:100)
+colorbar('Ytick', 0:20:100)
 
-fit1=fit(vx,vy,'poly1')
-[R,p]=corr(vx,vy);
+fit1 = fit(vx, vy, 'poly1')
+[R, p] = corr(vx, vy);
 disp ([R p])
-[R,p]=corr(vx,vy,'type','Spearman');
+[R, p] = corr(vx, vy, 'type', 'Spearman');
 disp ([R p])
